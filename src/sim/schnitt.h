cc29c1342e6b4a003d3e8b4527b1774b0e028036
Shiqing <shiqing-thu18@yandex.com>
2018-11-16 21:32:42 +0800

    :pencil2: Update schnitt function signatures

--- src/sim/schnitt.h
+++ src/sim/schnitt.h
@@ -4,8 +4,7 @@
 #define _SCHNITT_H
 
 void schnitt_apply(float x1, float y1, float x2, float y2);
-void schnitt_flush();
-void schnitt_get_delta(int dir, float *dx, float *dy);
+void schnitt_flush(float *dx, float *dy);
 
 #ifdef SCHNITT_TEST
 #include <stdbool.h>
