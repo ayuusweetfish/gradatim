43659720b0b09cdca4f656c230962c5507ed26d4
Shiqing <shiqing-thu18@yandex.com>
2018-11-23 17:40:52 +0800

    :bug: Ignore keyboard input during lead-in/failure

--- src/gameplay.c
+++ src/gameplay.c
@@ -413,6 +413,7 @@ static void gameplay_scene_key_handler(gameplay_scene *this, SDL_KeyboardEvent *
 } while (0)
 
     if (ev->repeat) return;
+    if (this->disp_state != DISP_NORMAL) return;
     switch (ev->keysym.sym) {
         case SDLK_c:
             if (ev->state == SDL_PRESSED) try_hop(this);
