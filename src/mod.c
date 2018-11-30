eb6302cf1fb25b22505c591555da8e4419814196
Shiqing <shiqing-thu18@yandex.com>
2018-11-30 12:09:50 +0800

    :pencil2: Rewrite modifier description

--- src/mod.c
+++ src/mod.c
@@ -1,24 +1,24 @@
 #include "mod.h"
 
 const struct mod_text MODS[N_MODS][N_MODSTATES] = {{
-    {"crotchet.png", 0x334455, "Moderato",
-     "Normal speed"},
-    {"quaver.png", 0x554433, "Vivace",
-     "Faster"},
-    {"minim.png", 0x335544, "Andante",
-     "Slower"}
+    {"crotchet.png", 0x445566, "Moderato",
+     "A tempo."},
+    {"quaver.png", 0x775544, "Vivace",
+     "A show-off of dexerity."},
+    {"minim.png", 0x446655, "Andante",
+     "Go on an unhurried adventure."}
 }, {
-    {"crotchet.png", 0x334455, "Moderato",
-     "Normal speed"},
-    {"quaver.png", 0x554433, "Vivace",
-     "Faster"},
-    {"minim.png", 0x335544, "Andante",
-     "Slower"}
+    {"crotchet.png", 0x445566, "Giusto",
+     "Feel the rhythm."},
+    {"quaver.png", 0x447777, "Rubato",
+     "Never restrict yourself."},
+    {"minim.png", 0x338888, "A piacere",
+     "Forget about all those rules!"}
 }, {
-    {"crotchet.png", 0x334455, "Moderato",
-     "Normal speed"},
-    {"quaver.png", 0x554433, "Vivace",
-     "Faster"},
-    {"minim.png", 0x335544, "Andante",
-     "Slower"}
+    {"crotchet.png", 0x445566, "Aria",
+     "Backed by the Muses."},
+    {"quaver.png", 0x333355, "A capella",
+     "Sightreading? Cakewalk."},
+    {"minim.png", 0x664466, "Stretto",
+     "Hearing is believing."}
 }};
