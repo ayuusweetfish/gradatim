60a932c4fecd7e52c9eb27c105eb765600ae58bb
Shiqing <shiqing-thu18@yandex.com>
2018-12-02 15:02:56 +0800

    :hankey: Somehow improve floue?

--- src/floue.c
+++ src/floue.c
@@ -1,16 +1,18 @@
 #include "floue.h"
 #include "global.h"
 
+#include <math.h>
 #include <stdlib.h>
 
 static const double SCALE = 4;
-static const int SZ_W = WIN_W / SCALE;
-static const int SZ_H = WIN_H / SCALE;
-static const int MARGIN = SZ_W / 3;
+static const double ACCEL = WIN_W / 20;
+static const double WANDER = 1;
+static const double V_MAX = WIN_W / 4;
 
-static const double TR_DUR = 1;
-static const double MAX_MOV = SZ_H / 8;
-static const double MAX_ALT = 10;
+static inline double random_abs(double max_abs)
+{
+    return (double)rand() / RAND_MAX * (2 * max_abs) - max_abs;
+}
 
 static inline double random_in(double l, double h)
 {
@@ -29,8 +31,11 @@ void floue_add(floue *this, SDL_Point p, SDL_Color c, int sz)
 {
     if (this->n >= FLOUE_CAP) return;
 
-    this->p[this->n] = p;
+    this->x[this->n] = p.x;
+    this->y[this->n] = p.y;
     this->sz[this->n] = sz;
+    this->vx[this->n] = random_abs(ACCEL / 4);
+    this->vy[this->n] = random_abs(ACCEL / 4);
 
     /* Create texture */
     sz /= SCALE;
@@ -65,6 +70,30 @@ void floue_drop(floue *this)
 
 void floue_tick(floue *this, double dt)
 {
+    int i;
+    for (i = 0; i < this->n; ++i) {
+        /* Update velocity */
+        double vx = this->vx[i], vy = this->vy[i];
+        double a = atan2(vy, vx) + random_abs(WANDER * dt);
+        double v = sqrt(sqr(vx) + sqr(vy));
+        double v_max = v + ACCEL * dt, v_min = v - ACCEL * dt;
+        if (v_max > V_MAX) v_max = V_MAX;
+        if (v_min < 0) v_min = 0;
+        v = random_in(v_min, v_max);
+        if (isnan(a)) a = random_abs(M_PI);
+        vx = v * cos(a);
+        vy = v * sin(a);
+        /* Update position */
+        this->vx[i] = vx;
+        this->vy[i] = vy;
+        this->x[i] += vx * dt;
+        this->y[i] += vy * dt;
+        int sz = this->sz[i];
+        if (this->x[i] < -sz / 2) this->x[i] += WIN_W + sz;
+        if (this->x[i] > WIN_W + sz / 2) this->x[i] -= WIN_W + sz;
+        if (this->y[i] < -sz / 2) this->y[i] += WIN_H + sz;
+        if (this->y[i] > WIN_H + sz / 2) this->y[i] -= WIN_H + sz;
+    }
 }
 
 void floue_draw(floue *this)
@@ -74,8 +103,8 @@ void floue_draw(floue *this)
     int i;
     for (i = 0; i < this->n; ++i)
         SDL_RenderCopy(g_renderer, this->tex[i], NULL, &(SDL_Rect){
-            this->p[i].x - this->sz[i] / 2,
-            this->p[i].y - this->sz[i] / 2,
+            this->x[i] - this->sz[i] / 2,
+            this->y[i] - this->sz[i] / 2,
             this->sz[i], this->sz[i]
         });
 }
--- src/floue.h
+++ src/floue.h
@@ -11,8 +11,9 @@ typedef struct _floue {
     SDL_Color c0;
     int n;
     SDL_Texture *tex[FLOUE_CAP];
-    SDL_Point p[FLOUE_CAP];
+    double x[FLOUE_CAP], y[FLOUE_CAP];
     int sz[FLOUE_CAP];
+    double vx[FLOUE_CAP], vy[FLOUE_CAP];
 } floue;
 
 floue *floue_create(SDL_Color c0);
