2cfe36e8835b9bff03d4e440a547023b91db2f3b
Shiqing <shiqing-thu18@yandex.com>
2018-12-03 18:01:01 +0800

    :lipstick: Add summary display at chapter finish

--- src/chapfin.c
+++ src/chapfin.c
@@ -10,22 +10,26 @@ static const double T1 = 0.7;
 static const double D3 = 1;
 static const double T2 = 1;
 static const double T2_TEXT_DUR = 0.4;
-static const double D4 = 1;
-static const double T3 = 0.7;
+
+static const double T3 = 0.5;
+static const double I3 = 0.02;
 
 /**
- *   \  | delay 1 |   fade   | delay2 |   txt1  | delay3 |    txt2    | delay4 |  txt3  |
- * Gmpl |         | cam move |        |         |        |            |        |        |
- * Imag |         |          |        |         |        | show, move |        |        |
- * Unvl |         |  fade in |        |         |        |   unveil   |        |        |
- * Text |         |          |        | fade in |        | fade, tint |        | result |
- * Line |         |          |        |       extend     |    tint    |        |        |
+ *   \  | delay 1 |   fade   | delay2 |   txt1  | delay3 |    txt2    |
+ * Gmpl |         | cam move |        |         |        |            |
+ * Imag |         |          |        |         |        | show, move |
+ * Unvl |         |  fade in |        |         |        |   unveil   |
+ * Text |         |          |        | fade in |        | fade, tint |
+ * Line |         |          |        |       extend     |    tint    |
  **/
 
 static const double CAM_MOVE = 4;
 static const double LINE_X = 0.15;
 static const double LINE_Y = 0.4;
 static const double LINE_WIDTH = 4;
+static const double ICON_BASE_X = 0.75;
+static const double ICON_SZ = 0.05;
+static const double ICON_REAL_SZ = 0.04;
 
 static void chapfin_tick(chapfin_scene *this, double dt)
 {
@@ -34,10 +38,46 @@ static void chapfin_tick(chapfin_scene *this, double dt)
     this->time += dt;
 }
 
+static inline void update_summary_obj(sprite *s, double x, double y, double t)
+{
+    if (t < 0) return;
+    double r = (t > T3 ? 1 : t / T3);
+    r = 1 - (1 - r) * (1 - r) * (1 - r) * (1 - r);
+    /*r = ease_elastic_out(r, 0.9);*/
+    element_place((element *)s, round(WIN_W * x), round(WIN_H * (y + 0.1 * (1 - r))));
+    s->alpha = round(r * 255);
+}
+
 static void chapfin_draw(chapfin_scene *this)
 {
     double u_opacity = 0;
-    if (this->time < D1 + F + D2) {
+    if (this->summary_shown) {
+        SDL_SetRenderDrawColor(g_renderer, 255, 192, 192, 255);
+        SDL_RenderClear(g_renderer);
+        floue_draw(this->f);
+        int i;
+        for (i = 0; i < N_MODS; ++i)
+            update_summary_obj((sprite *)this->mod_icon[i],
+                ICON_BASE_X - ICON_SZ * (N_MODS - i), 0.7, this->time - I3 * i);
+        update_summary_obj((sprite *)this->l_summary,
+            ICON_BASE_X, 0.7, this->time - I3 * N_MODS);
+        update_summary_obj((sprite *)this->clock,
+            ICON_BASE_X - ICON_SZ, 0.77, this->time - I3 * (N_MODS + 1));
+        update_summary_obj((sprite *)this->l_timer,
+            ICON_BASE_X, 0.77, this->time - I3 * (N_MODS + 2));
+        update_summary_obj((sprite *)this->l_timer_d,
+            (double)(this->l_timer->_base._base.dim.x + this->l_timer->_base._base.dim.w) / WIN_W,
+            0.77, this->time - I3 * (N_MODS + 3));
+        update_summary_obj((sprite *)this->mushroom,
+            ICON_BASE_X - ICON_SZ, 0.84, this->time - I3 * (N_MODS + 4));
+        update_summary_obj((sprite *)this->l_retries,
+            ICON_BASE_X, 0.84, this->time - I3 * (N_MODS + 5));
+        SDL_SetRenderDrawColor(g_renderer, 255, 255, 255, 255);
+        SDL_RenderFillRect(g_renderer, &(SDL_Rect){
+            WIN_W * LINE_X, WIN_H * LINE_Y,
+            WIN_W * (1 - LINE_X * 2), LINE_WIDTH
+        });
+    } else if (this->time < D1 + F + D2) {
         if (this->time >= D1 && this->time < D1 + F) {
             double r = (this->time - D1) / F;
             this->g->cam_y = this->orig_cam_y - r * r * CAM_MOVE;
@@ -61,7 +101,7 @@ static void chapfin_draw(chapfin_scene *this)
             WIN_W * x, WIN_H * LINE_Y,
             WIN_W * (1 - x * 2), LINE_WIDTH
         });
-    } else if (this->time < D1 + F + D2 + T1 + D3 + T2 + D4) {
+    } else if (this->time < D1 + F + D2 + T1 + D3 + T2) {
         double r = (this->time - (D1 + F + D2 + T1 + D3)) / T2;
         if (r > 1) r = 1;
         double s = (this->time - (D1 + F + D2 + T1 + D3)) / T2_TEXT_DUR;
@@ -80,14 +120,11 @@ static void chapfin_draw(chapfin_scene *this)
             WIN_W * (1 - LINE_X * 2), LINE_WIDTH
         });
     } else {
-        double r = (this->time - (D1 + F + D2 + T1 + D3 + T2 + D4)) / T3;
-        if (r > 1) r = 1;
         SDL_SetRenderDrawColor(g_renderer, 255, 192, 192, 255);
         SDL_RenderClear(g_renderer);
         floue_draw(this->f);
         label_colour_mod(this->l_num, 255, 255, 255);
         label_colour_mod(this->l_title, 255, 255, 255);
-        this->l_extra->_base.alpha = round(r * 255);
         SDL_SetRenderDrawColor(g_renderer, 255, 255, 255, 255);
         SDL_RenderFillRect(g_renderer, &(SDL_Rect){
             WIN_W * LINE_X, WIN_H * LINE_Y,
@@ -117,10 +154,15 @@ static void exit_postroutine(chapfin_scene *this, void *unused)
 static void chapfin_key(chapfin_scene *this, SDL_KeyboardEvent *ev)
 {
     if (ev->state != SDL_PRESSED) return;
-    if (this->time < D1 + F + D2 + T1 + D3 + T2 + D4) return;
-    g_stage = (scene *)loading_create(
-        &g_stage, (loading_routine)exit_routine,
-        (loading_postroutine)exit_postroutine, this);
+    if (this->time < D1 + F + D2 + T1 + D3 + T2) return;
+    if (!this->summary_shown) {
+        this->summary_shown = true;
+        this->time = 0;
+    } else {
+        g_stage = (scene *)loading_create(
+            &g_stage, (loading_routine)exit_routine,
+            (loading_postroutine)exit_postroutine, this);
+    }
 }
 
 chapfin_scene *chapfin_scene_create(gameplay_scene *g)
@@ -136,6 +178,7 @@ chapfin_scene *chapfin_scene_create(gameplay_scene *g)
     this->orig_cam_y = g->cam_y;
     this->u = unveil_create();
     this->f = floue_create((SDL_Color){0});
+    this->summary_shown = false;
 
     int i;
     for (i = 0; i < 6; ++i)
@@ -160,11 +203,49 @@ chapfin_scene *chapfin_scene_create(gameplay_scene *g)
     this->l_title = l;
 
     l = label_create("KiteOne-Regular.ttf", 28,
-        (SDL_Color){255, 255, 255}, WIN_W, "14:46:40.412");
-    element_place_anchored((element *)l, WIN_W / 2, WIN_H * 0.8, 0.5, 0.5);
+        (SDL_Color){255, 255, 255}, WIN_W, "Complete run");
     l->_base.alpha = 0;
     bekter_pushback(this->_base.children, l);
-    this->l_extra = l;
+    this->l_summary = l;
+
+    l = label_create("KiteOne-Regular.ttf", 28,
+        (SDL_Color){255, 255, 255}, WIN_W, "015.  07.");
+    l->_base.alpha = 0;
+    bekter_pushback(this->_base.children, l);
+    this->l_timer = l;
+
+    l = label_create("KiteOne-Regular.ttf", 21,
+        (SDL_Color){255, 255, 255}, WIN_W, "47");
+    l->_base.alpha = 0;
+    bekter_pushback(this->_base.children, l);
+    this->l_timer_d = l;
+
+    l = label_create("KiteOne-Regular.ttf", 28,
+        (SDL_Color){255, 255, 255}, WIN_W, "4073");
+    l->_base.alpha = 0;
+    bekter_pushback(this->_base.children, l);
+    this->l_retries = l;
+
+    sprite *sp = sprite_create("clock.png");
+    sp->_base.dim.w = sp->_base.dim.h = WIN_W * ICON_REAL_SZ;
+    sp->alpha = 0;
+    bekter_pushback(this->_base.children, sp);
+    this->clock = sp;
+
+    sp = sprite_create("retry_count.png");
+    sp->_base.dim.w = sp->_base.dim.h = WIN_W * ICON_REAL_SZ;
+    sp->alpha = 0;
+    bekter_pushback(this->_base.children, sp);
+    this->mushroom = sp;
+
+    for (i = 0; i < N_MODS; ++i) {
+        int idx = (this->g->mods >> (i * 2)) & 3;
+        sp = sprite_create(MODS[i][idx > 2 ? 2 : idx].icon);
+        sp->_base.dim.w = sp->_base.dim.h = WIN_W * ICON_REAL_SZ;
+        sp->alpha = 0;
+        bekter_pushback(this->_base.children, sp);
+        this->mod_icon[i] = sp;
+    }
 
     return this;
 }
--- src/chapfin.h
+++ src/chapfin.h
@@ -4,10 +4,14 @@
 #define _CHAPFIN_H
 
 #include "scene.h"
+#include "element.h"
 #include "label.h"
 #include "gameplay.h"
+#include "mod.h"
 #include "unveil.h"
 #include "floue.h"
+
+#include <stdbool.h>
 
 typedef struct _chapfin_scene {
     scene _base;
@@ -20,7 +24,12 @@ typedef struct _chapfin_scene {
     unveil *u;
     floue *f;
     label *l_num, *l_title;
-    label *l_extra;
+
+    bool summary_shown;
+    sprite *mod_icon[N_MODS];
+    label *l_summary;
+    sprite *clock, *mushroom;
+    label *l_timer, *l_timer_d, *l_retries;
 } chapfin_scene;
 
 chapfin_scene *chapfin_scene_create(gameplay_scene *g);
