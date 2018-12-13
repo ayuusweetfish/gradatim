5d59cc4d9f25dc0863215194a5d9ac0aa7e6bfb8
Shiqing <shiqing-thu18@yandex.com>
2018-12-13 15:21:40 +0800

    :bug: Fix incorrect text region height in credits

--- src/credits.c
+++ src/credits.c
@@ -12,7 +12,7 @@ static const double V0 = 50;
 static const double V = 960;
 static const double A = 2880;
 #define RANGE_W (WIN_W * 0.8)
-#define RANGE_H (WIN_W * 0.6)
+#define RANGE_H (WIN_H * 0.6)
 static const int OFFS_X = (WIN_W - RANGE_W) / 2;
 static const int OFFS_Y = WIN_H * 0.232;
 
